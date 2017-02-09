9d2aa587cc4de0996cfc01161349ec53c63214d2
nealehemrajani <nealehemrajani@gmail.com>
2017-02-08 20:41:24 -0800

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Neale Hemrajani on 2/8/17.
+//  Copyright © 2017 Neale Hemrajani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
